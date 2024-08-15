50727d25629cf458ec33d43bc1718869c687546c
taetse <taetse.durand@gmail.com>
2024-08-15 18:37:11 +0200

    use chunked operations vs streams

--- pkg/middlewares/tcp/streamcompress/zstd.go
+++ pkg/middlewares/tcp/streamcompress/zstd.go
@@ -1,12 +1,9 @@
 package tcpstreamcompress
 
 import (
-	"fmt"
+	"bytes"
 	"github.com/klauspost/compress/zstd"
 	"github.com/traefik/traefik/v3/pkg/tcp"
-	"io"
-	"sync"
-	"time"
 )
 
 // Take compressed data from upstream and send it plain to backend
@@ -65,113 +62,105 @@ func (z *zstdDecompressor) CloseWrite() error {
 }
 
 // Take decompressed data from upstream and send it compressed
+
 type zstdCompressor struct {
 	tcp.WriteCloser
 
-	compressor_r   *io.PipeReader
-	decompressor_w *io.PipeWriter
+	reader *zstd.Decoder
+	writer *zstd.Encoder
 
-	decompressor   *zstd.Decoder
-	compressor     *zstd.Encoder
-	writeWaitGroup sync.WaitGroup
-	waitGroup      sync.WaitGroup
+	readBuffer  bytes.Buffer
+	writeBuffer bytes.Buffer
+	chuckSize   int
 }
 
 func NewZStdCompressor(conn tcp.WriteCloser, level zstd.EncoderLevel, dict []byte) tcp.WriteCloser {
 	z := &zstdCompressor{
-		WriteCloser:    conn,
-		writeWaitGroup: sync.WaitGroup{},
-		waitGroup:      sync.WaitGroup{},
+		WriteCloser: conn,
+		chuckSize:   4 * 1024,
 	}
 
 	var err error
-	compressor_r, compressor_w := io.Pipe()
-	encoderOptions := make([]zstd.EOption, 0)
+	encoderOptions := []zstd.EOption{zstd.WithEncoderLevel(level), zstd.WithZeroFrames(true)}
+	decoderOptions := []zstd.DOption{}
+
 	if dict != nil {
 		encoderOptions = append(encoderOptions, zstd.WithEncoderDict(dict))
-	}
-	encoderOptions = append(encoderOptions, zstd.WithEncoderLevel(level))
-	z.compressor, err = zstd.NewWriter(compressor_w, encoderOptions...)
-	if err != nil {
-		panic(err)
-	}
-
-	// TODO: I'm not particularly happy about these being goroutines, IMO they
-	// should fit just fine into the Read/Write functions but it seemed to add
-	// a lot of code overhead. Perhaps there is a library for it?
-	z.waitGroup.Add(1)
-	go func() {
-		defer func() {
-			compressor_w.CloseWithError(io.EOF)
-			compressor_r.CloseWithError(io.EOF)
-			z.compressor.Close()
-			z.waitGroup.Done()
-		}()
-
-		tmp := make([]byte, 32*1024)
-		for {
-			// read from conn and write to compressor. Cannot use io.Copy because it will not flush
-			n, err := conn.Read(tmp)
-			fmt.Printf("Compressor read %d bytes: %s\n", n, tmp[0:n])
-			if err != nil {
-				return
-			}
-			if n == 0 {
-				continue
-			}
-			n, err = z.compressor.Write(tmp[0:n])
-			if err != nil {
-				return
-			}
-			// Have to flush each time to get the zstd flush block
-			z.compressor.Flush()
-		}
-	}()
-
-	decompressor_r, decompressor_w := io.Pipe()
-	decoderOptions := make([]zstd.DOption, 0)
-	if dict != nil {
 		decoderOptions = append(decoderOptions, zstd.WithDecoderDicts(dict))
 	}
-	z.decompressor, err = zstd.NewReader(decompressor_r, decoderOptions...)
+
+	z.writer, err = zstd.NewWriter(nil, encoderOptions...)
 	if err != nil {
 		panic(err)
 	}
-	z.writeWaitGroup.Add(1)
-	go func() {
-		defer func() {
-			decompressor_w.CloseWithError(io.EOF)
-			decompressor_r.CloseWithError(io.EOF)
-			z.decompressor.Close()
-			z.writeWaitGroup.Done()
-		}()
-		io.Copy(conn, z.decompressor)
-	}()
 
-	z.compressor_r = compressor_r
-	z.decompressor_w = decompressor_w
+	z.reader, err = zstd.NewReader(nil, decoderOptions...)
+	if err != nil {
+		panic(err)
+	}
 
 	return z
 }
+
 func (z *zstdCompressor) Read(p []byte) (n int, err error) {
-	return z.compressor_r.Read(p)
+	if z.readBuffer.Len() == 0 {
+		unCompressedData := make([]byte, z.chuckSize)
+		n, err = z.WriteCloser.Read(unCompressedData)
+		if err != nil {
+			return 0, err
+		}
+
+		compressedData := z.writer.EncodeAll(unCompressedData[:n], make([]byte, 0))
+		z.readBuffer.Write(compressedData)
+	}
+
+	return z.readBuffer.Read(p)
 }
+
 func (z *zstdCompressor) Write(p []byte) (n int, err error) {
-	return z.decompressor_w.Write(p)
+	n, err = z.writeBuffer.Write(p)
+	if err != nil {
+		return 0, err
+	}
+
+	if z.writeBuffer.Len() > z.chuckSize {
+		err = z.Flush()
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return n, err
+}
+
+func (z *zstdCompressor) Flush() error {
+	if z.writeBuffer.Len() == 0 {
+		return nil
+	}
+	compressedData := z.writeBuffer.Bytes()
+	z.writeBuffer.Reset()
+	decompressedData, err := z.reader.DecodeAll(compressedData, make([]byte, 0, z.chuckSize))
+	if err != nil {
+		return err
+	}
+
+	_, err = z.WriteCloser.Write(decompressedData)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (z *zstdCompressor) Close() error {
-	z.decompressor_w.CloseWithError(io.EOF)
-	z.writeWaitGroup.Wait()
-
-	z.WriteCloser.SetReadDeadline(time.Now())
-	z.waitGroup.Wait()
-
+	z.Flush()
+	z.writer.Close()
+	z.reader.Close()
 	return z.WriteCloser.Close()
 }
-func (z *zstdCompressor) CloseWrite() error {
-	z.decompressor_w.CloseWithError(io.EOF)
-	z.writeWaitGroup.Wait()
 
+func (z *zstdCompressor) CloseWrite() error {
+	z.Flush()
+	z.writer.Close()
 	return z.WriteCloser.CloseWrite()
 }
