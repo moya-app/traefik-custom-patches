c0583e47e6bc26c490def571b2796acda45d8d58
Tinashe Chikomo <tinashe@chikomo.net>
2025-01-29 22:05:35 +0200

    g

--- script/make.sh
+++ script/make.sh
@@ -6,12 +6,7 @@ export GOPROXY=https://proxy.golang.org
 
 # List of bundles to create when no argument is passed
 DEFAULT_BUNDLES=(
-	generate
-	validate-lint
 	binary
-
-	test-unit
-	test-integration
 )
 
 SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
